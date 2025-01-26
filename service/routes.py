de2917edf1535f0fbff332f63d5009328b706c0c
Ray Botkin <raykdata@gmail.com>
2025-01-26 09:17:28 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,15 +57,12 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-        """
-        List all Accounts
-        This endpoint will list all Accounts
-        """
         app.logger.info("Request to list Accounts")
         accounts = Account.all()
         account_list = [account.serialize() for account in accounts]
@@ -78,25 +75,18 @@ def list_accounts():
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
-    """
-    Reads an Account
-    This endpoint will read an Account based the account_id that is requested
-    """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+    
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
-        """
-        Update an Account
-        This endpoint will update an Account based on the posted data
-        """
         app.logger.info("Request to update an Account with id: %s", account_id)
         account = Account.find(account_id)
         if not account:
